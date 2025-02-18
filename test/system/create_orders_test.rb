053bd4e9f9e998a2c17bd94fc67d92119f664caa
denialtorres <denial.torres@gmail.com>
2025-02-17 20:21:33 -0600

    fix tests

--- test/system/create_orders_test.rb
+++ test/system/create_orders_test.rb
@@ -34,6 +34,8 @@ class CreateOrdersTest < ApplicationSystemTestCase
     fill_in "order[quantity]", with: 2
 
     click_on "Place Order" # place paid order
+    Sidekiq::Job.drain_all
+    refresh
 
     refute_selector "aside[data-error]"
     order = Order.last
@@ -65,6 +67,9 @@ class CreateOrdersTest < ApplicationSystemTestCase
     fill_in "order[quantity]", with: 1
 
     click_on "Place Order" # place declined order
+
+    Sidekiq::Job.drain_all
+    refresh
 
     assert_text "Payment Declined: Insufficient funds"
     order = Order.last
